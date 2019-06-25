af6904b79d525c817fa9f6ad54c5cba64cd90424
Jules <Faivre.jules@gmail.com>
2019-06-25 15:10:36 +0200

    display date header festival

--- src/Components/OnePage/PageHeaderComp/OnePageHeader.jsx
+++ src/Components/OnePage/PageHeaderComp/OnePageHeader.jsx
@@ -7,27 +7,37 @@ function getPageAdress(type, page, id) {
   return `/edit/${type}/${page}/${id}`;
 }
 
+function splitDate(oneDate) {
+  const formated = oneDate.split("T");
+  return formated[0]
+    .split("-")
+    .reverse()
+    .join("-");
+}
+
 const OnePageHeader = props => {
-  console.log(props.pageInfos);
+  const { pageInfos, modifyState } = props;
   let noShow;
-  if (!props.modifyState) noShow = { display: "none" };
+  if (!modifyState) noShow = { display: "none" };
   return (
     <div className={classes.onePageHeader}>
       <img
-        alt={props.pageInfos.profile_picture_alt}
+        alt={pageInfos.profile_picture_alt}
         src={`https://s3.eu-west-3.amazonaws.com/festview/${
-          props.pageInfos.profile_picture_file
+          pageInfos.profile_picture_file
         }`}
       />
-      <h1>{props.pageInfos.name}</h1>
-      <h3>@{props.pageInfos.pseudo}</h3>
+      <h1>{pageInfos.name}</h1>
+      <h3>@{pageInfos.pseudo}</h3>
+      {pageInfos.title === "festival" ? (
+        <h3>
+          {splitDate(pageInfos.event_starting_date)} /{" "}
+          {splitDate(pageInfos.event_ending_date)}
+        </h3>
+      ) : null}
       <div className={classes.bgc} style={noShow}>
         <Link
-          to={getPageAdress(
-            props.pageInfos.title,
-            props.pageInfos.pseudo,
-            props.pageInfos.id
-          )}
+          to={getPageAdress(pageInfos.title, pageInfos.pseudo, pageInfos.id)}
         >
           <img className={classes.custIcon} src={plusIcon} alt="modify icon" />
         </Link>
