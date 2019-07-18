7f6e947d5784f5e771d96804ef94801a36884a34
Jules <Faivre.jules@gmail.com>
2019-07-18 13:40:22 +0200

    Cleaner filter at Allpages

--- src/Components/AllPages/AllPages/AllPages.jsx
+++ src/Components/AllPages/AllPages/AllPages.jsx
@@ -2,9 +2,17 @@ import React from "react";
 import classes from "./AllPages.css";
 import AllPageComp from "../AllPagesComp/AllPagesComp";
 
+const getTitle = props => {
+  const dataArray = props.dataList.filter(
+    oneArtist =>
+      oneArtist.title ===
+      props.match.path.slice(4, props.match.path.length - 1).toLowerCase()
+  );
+  const customTitle = props.match.path.slice(4);
+  return [dataArray, customTitle];
+};
+
 const AllPages = props => {
-  let dataArray = [];
-  let customTitle = "";
   if (!props.dataList) {
     return (
       <div className={classes.mainDiv}>
@@ -12,22 +20,11 @@ const AllPages = props => {
       </div>
     );
   }
-  if (props.match.path === "/AllArtists") {
-    dataArray = props.dataList.filter(
-      oneArtist => oneArtist.title === "artist"
-    );
-    customTitle = "Here is our Artists list:";
-  } else if (props.match.path === "/AllFestivals") {
-    dataArray = props.dataList.filter(
-      oneFestival => oneFestival.title === "festival"
-    );
-    customTitle = "Here is our Festivals list:";
-  }
   return (
     <div className={classes.mainDiv}>
-      <h3>{customTitle}</h3>
+      <h3>{getTitle(props)[1]}</h3>
       <ul>
-        {dataArray.map(oneData => {
+        {getTitle(props)[0].map(oneData => {
           return (
             <AllPageComp
               key={oneData.slug}
