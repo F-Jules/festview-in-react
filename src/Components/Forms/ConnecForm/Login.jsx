aa13dd983d0c056a0c67275eaada82bc9e580c0d
Jules Faivre <faivre.jules@gmail.com>
2019-09-11 22:44:10 +0200

    Login ?

--- src/Components/Forms/ConnecForm/Login.jsx
+++ src/Components/Forms/ConnecForm/Login.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import APIHandler from "../../../Api/ApiHandler";
 import { Link } from "react-router-dom";
 import classes from "../form.css";
 import InputFrom from "../Composants/Input/InputForm";
@@ -7,30 +8,28 @@ import TitleFormCo from "../Composants/TitleForm/TitleFormCo";
 import CreateButton from "../Composants/Buttons/CreateAccBut";
 import FeedBack from "./FeedBack";
 
+const ApiHandler = new APIHandler();
+
 class Login extends Component {
   state = {};
 
+  handlePost = evt => {
+    evt.preventDefault();
+    console.log(this.state);
+  };
+
   handleInput = evt => {
+    evt.preventDefault();
     this.setState({ [evt.target.name]: evt.target.value });
   };
+
   render() {
-    console.log(typeof this.props.location.state);
     return (
       <div className={classes.form}>
         <TitleFormCo text="Connexion" />
-        <form action="post">
-          <InputFrom
-            text="Email"
-            type="email"
-            name="email"
-            handleInput={this.handleInput}
-          />
-          <InputFrom
-            text="Mot de passe"
-            type="password"
-            name="password"
-            handleInput={this.handleInput}
-          />
+        <form onSubmit={this.handlePost} onChange={this.handleInput}>
+          <InputFrom text="Email" type="email" name="email" />
+          <InputFrom text="Mot de passe" type="password" name="password" />
           <Button text="Me Connecter" />
         </form>
         {this.props.location.state === undefined ? (
